5ae96aa13166ef5437bf804dba6a0a5763747c40
spurti-singaraddi <spurticsingaraddi26@gmail.com>
2021-08-25 14:48:33 +0530

    added all files

---
+++ src/main/java/com/test/project/MavenProject2/App.java
@@ -0,0 +1,7 @@
+package com.test.project.MavenProject2;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/com/test/project/MavenProject2/AppTest.java
@@ -0,0 +1,13 @@
+package com.test.project.MavenProject2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
